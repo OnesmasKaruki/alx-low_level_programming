2c89dba2a357a2d6823117e3b7321f83dabb6f6d
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-05 08:26:04 -0700

    Task_18

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,12 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			_putchar("%d, ", n--);
+			_putchar('%d, ', n--);
 	}
 
 	else
 	{
 		while (n < 98)
-			_putchar("%d, ", n++);
+			_putchar('%d, ', n++);
 	}
 }
