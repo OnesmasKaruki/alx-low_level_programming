916dcea8cddc3247a83c119de41056e0f31334d6
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-05 08:22:58 -0700

    Task_11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 -> print all natural numbers from input to 98
+ * @n: The number being counting at.
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			_putchar("%d, ", n--);
+	}
+
+	else
+	{
+		while (n < 98)
+			_putchar("%d, ", n++);
+	}
+}
