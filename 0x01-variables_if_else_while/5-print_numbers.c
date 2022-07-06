11ea7b4d40d7486a4933a13c64b2d39c5561c4ad
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-06 06:30:11 -0700

    adaigit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - paramenter passed
+ *
+ * @n: Parameter
+ * Return: Always return n
+ */
+int main(int n)
+{
+	for (n = 0; n <= 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (n);
+}
