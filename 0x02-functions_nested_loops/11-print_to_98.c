5014fd668cd7b6c84558dd064db0b6e378d9d61f
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-06 15:02:38 -0700

    Do

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,19 +17,19 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-			printf(",");
+			printf("%d", n);
 		}
 		printf("\n");
 	}
 	else
 	{
-		for (n = 0; n >= 98; n++)
+		for (n = 0; n >= 98; n--)
 		{
 			printf("%d", n);
 
 			if (n == 98)
 				continue;
-			printf(",");
+			printf("%d", n);
 		}
 		printf("\n");
 	}
