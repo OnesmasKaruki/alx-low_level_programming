cb59a243a464a0c4127dee2e4f44e817974157cb
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-06 01:43:24 -0700

    Error fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,13 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while (n > 98)
-			_putchar('%d, ', n--);
+		while (n > 98; n--)
+			_putchar(n);
 	}
 
 	else
 	{
-		while (n < 98)
-			_putchar('%d, ', n++);
+		while (n < 98; n++)
+			_putchar(n);
 	}
 }
