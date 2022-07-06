be4afa779386d1f0d32d5d56cb90813d382da57c
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-06 07:23:05 -0700

    task_6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -> prints number
+ *
+ * Return: ALways return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar("%d", i);
+	}
+	putchar("\n");
+	return (0);
+}
