909d9b15d86bdb8287be1ee7bd4119bd6eb30269
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-07 09:00:38 -0700

    uPPERCASE

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - thiis function to check uppercase
+ * @c: Parameter passed
+ * Return: Sometthing but Success
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
