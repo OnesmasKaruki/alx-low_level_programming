69630eff445155e9ad22f33114a464f05e9665bf
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-11 07:27:46 -0700

    Print array element

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - prints n ellement of array of integer
+ * @n: Number of element of array
+ * @a: Address
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int arr[n], i, n;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(arr[i]);
+		_putchar('\n');
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-Function that prints half of a string
+Function that prints n elements of elements of the array to be printed
