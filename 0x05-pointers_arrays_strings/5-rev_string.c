7d2e01c6c644c16137e0fc4e23f1e85e30b03cf8
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-11 04:27:44 -0700

    Function reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,28 +8,20 @@
 
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
+	int len, i, half;
+	char temp;
 
-	a = s;
-
-	while (s[c] != '\0')
+	for (len = 0; s[len] != '\0'; len++)
 	{
-		c++;
-	}
+		i = 0;
+		half = len / 2;
 
-	for (k = 1; k < c; k++)
-	{
-		a++;
+		while (half--)
+		{
+			temp = s[len - i - 1];
+			s[len - i - 1] = s[i];
+			s[i] = temp;
+			i++;
+		}
 	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-		_putchar(s);
-	}
-	_putchar('\n');
 }
