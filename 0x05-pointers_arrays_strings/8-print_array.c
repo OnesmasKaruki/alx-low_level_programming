5329c3b85e024ef087c9ea3c123cdfb9b7a6f4c1
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-11 09:21:53 -0700

    Finalize

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,14 @@
 void print_array(int *a, int n)
 {
 	int j;
+
 	for (j = 0; j < n; j++)
 	{
-		_putchar(a[j]);
+		_putchar('0' + a[j]);
+		if (j != (n - 1))
+		{
+			_putchar(",");
+		}
 	}
 	_putchar('\n');
 }
