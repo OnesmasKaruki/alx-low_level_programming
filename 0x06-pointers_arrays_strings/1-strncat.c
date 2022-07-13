ce96f76cb78e34f59fe21e49f3ffbb17b022ebd8
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-13 09:37:31 -0700

    Concantenate ss

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncat - concatenate n bytes of two strings
@@ -10,16 +11,7 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, i;
-
-	for (dest_len = 0; dest[dest_len] != '\0';dest_len++)
-	{
-		for (i = 0; i < n && src[i] != '\0'; i++)
-			dest[dest_len + i] = src[i];
-	}
-
-	/* Should end with a end of string char*/
-	dest[dest_len +i] = '\0';
+	strncat(dest, src, n);
 
 	return (dest);
 }
