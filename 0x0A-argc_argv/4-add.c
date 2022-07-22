399038d21c429236356da0756b28337a12148a5c
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-22 12:21:03 -0700

    Final

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds posutie integers
+ * @argc: count of string arguments in argv
+ * @argv: input string arguments
+ *
+ * Return: 0 on success 1 o0 failure
+ */
+int main(int argc, char *argv[])
+{
+	int num, num2, sum;
+
+	sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (num = 1; num < argc; num++)
+	{
+		for (num2 = 0; argv[num][num2] != '\0'; num2++)
+		{
+			if (argv[num][num2] < '0' || argv[num][num2] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[num]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
