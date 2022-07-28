5783d79378a036be24282a8f1252825d98cbb6df
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-07-28 10:54:16 -0700

    First commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: Memory allocated to
+ * Return: Pointer for Success
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exist(98);
+	return (ptr);
+}
