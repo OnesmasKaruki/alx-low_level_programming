8c10005de28e7072c80358fdf03f2451ad59bad3
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-08-01 08:07:49 -0700

    New_commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Desciption: act as a coumpud data typ.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef dog dog_h;
+int main()
+{
+	dog_h.name;
+	dog_h.age;
+	dog_h.owner;
+
+	return (0);
+}
