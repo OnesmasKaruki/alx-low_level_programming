a55236088020301fc5000291c38572f34226c587
OnesmasKaruki <kariukionesmus01@gmail.com>
2022-08-18 04:37:01 -0700

    Binary_Int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,17 +2,26 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts binary numbers to int
+ * binary_to_uint - converts binary numbers to int
  * @b: binary input to be converted
  *
  * Return: the converted number or 0 if ...
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	int num;
+	int i = 0;
+	unsigned int sum = 0;
 
-	if (num < 2)
-		return (b);
-	else
-		return (NULL);
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		sum = sum << 1;
+
+		if (b[i] == '1')
+			sum += 1;
+	}
+	return (sum);
 }
